c43b7ce468957baa5ab01a13d4492b20b9c16d20
Phil Crissman <phcrissm@microsoft.com>
2023-12-07 09:38:51 -0800

    Part 2 of day 4

--- day4.rb
+++ day4.rb
@@ -7,4 +7,16 @@ data =  File.read("./day4.txt").split("\n").map{|line| line.split(":")}.to_h.map
 # part 1
 puts data.map{|k,v| 2 ** ((v[0] & v[1]).size - 1) }.select{|n| n>=1}.reduce(:+)
 
-data.map{|k,v| (v[0] & v[1]).size }
+# part 2
+cards = data.map{|k,v| [(v[0] & v[1]).size, 1] }
+
+cards.map.with_index do |scratchcards, i|
+  (1..scratchcards[0]).each do |n|
+    break if i+n >= cards.size
+    cards[i+n][1] += scratchcards[1]
+  end
+  
+end
+
+puts cards.map{|el| el[1]}.reduce(:+)
+
